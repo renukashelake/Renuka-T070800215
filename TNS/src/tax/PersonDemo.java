492e80c3c89781c24101445b8da1bc55e44901ed
renukashelake <124230859+renukashelake@users.noreply.github.com>
2024-04-04 11:28:37 +0530

    tax

---
+++ TNS/src/tax/PersonDemo.java
@@ -0,0 +1,32 @@
+package tax;
+import java.util.*; //ctrl+shift+o
+
+public class PersonDemo {
+
+	public static void main(String[] args) {
+		//create scanner object
+		Scanner ob=new Scanner(System.in);
+		System.out.println("enter person name");
+		String name=ob.next();
+		System.out.println("enter age");
+		int age=ob.nextInt();
+		System.out.println("enter Taxable Income");
+		int income=ob.nextInt();
+		System.out.println("enter gender");
+		String gender=ob.next();
+		
+		//person object and initialize
+		Person person=new Person();
+		person.setName(name);
+		person.setAge(age);
+		person.setGender(gender);
+		person.setIncome(income);
+		
+		TaxCalculation tx=new TaxCalculation();
+		tx.calculateTx(person);
+		
+		System.out.println(person);
+
+	}
+
+}
---
+++ TNS/src/tax/TaxCalculation.java
@@ -0,0 +1,34 @@
+package tax;
+
+public class TaxCalculation {
+
+	public void calculateTx(Person person)
+	{
+		if(person.getAge()>65 || person.getGender().equalsIgnoreCase("female"))
+		{
+			person.setTax(0);
+			System.out.println("Tax not Applicable");
+		}
+		else
+		{
+			if(person.getIncome() <=160000)
+			{
+				person.setTax(0);
+			}
+			else if(person.getIncome() > 160000 && person.getIncome()<=500000)
+			{
+				person.setTax((person.getIncome()- 160000 ) * 5/100);
+			}
+			else if(person.getIncome() > 500000 && person.getIncome()<=800000)
+			{
+				person.setTax((person.getIncome()- 160000 )* 10/100);
+			}
+			else
+			{
+				person.setTax((person.getIncome()-160000)* 20/100);
+			}
+			
+		}
+	}
+
+}
